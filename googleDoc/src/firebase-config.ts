04202c60e29e1c029bff098d7e48b7315832dee9
Matheus Cardoso <46722962+MatheusCTorres@users.noreply.github.com>
2024-12-19 10:23:10 +0100

    initial

---
+++ googleDoc/src/firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC19hcDPeqqvL93QfozBzWBn490N6bVPs0",
+  authDomain: "docs-clone-e2460.firebaseapp.com",
+  projectId: "docs-clone-e2460",
+  storageBucket: "docs-clone-e2460.firebasestorage.app",
+  messagingSenderId: "988907903989",
+  appId: "1:988907903989:web:537b83ac895291d60a1ebc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
